cbec13f4797b145d2d329a77dcf2855dc1476139
nikibasil <nikitbasil@gmail.com>
2023-01-05 20:35:52 -0700

    Prep One and Prep 2 assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,51 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    Console.Write("What is your grade percentage? ");
+    string percentage = Console.ReadLine();
+    int number = int.Parse(percentage);
+    string grade = "";
+
+    if (number >= 90)
     {
-        Console.WriteLine("Hello Prep2 World!");
+      grade = "A";
+      //Console.WriteLine("Your grade is an A");
     }
+    else if (number >= 80)
+    {
+      grade = "B";
+      // Console.WriteLine("Your grade is a B");
+    }
+    else if (number >= 70)
+    {
+      grade = "C";
+      //Console.WriteLine("Your grade is a C");
+    }
+    else if (number >= 60)
+    {
+      grade = "D";
+      //Console.WriteLine("Your grade is a D");
+    }
+    else
+    {
+      grade = "F";
+      //Console.WriteLine("Your grade is an F");
+    }
+
+    if (number >= 70)
+    {
+      Console.WriteLine($"Your grade is a {grade}.");
+      Console.WriteLine("Congratulations you passed the class!");
+    }
+
+    else
+    {
+      Console.WriteLine($"Your grade is a {grade}.");
+      Console.WriteLine("Sorry you did not pass the class. Keep working at it!");
+    }
+  }
+
+
 }
